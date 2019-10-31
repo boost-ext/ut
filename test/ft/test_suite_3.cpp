e00fe49b6fd9a2876b102ff64fae77412cbab11c
Kris Jusiak <kris@jusiak.net>
2019-10-31 11:35:57 -0600

    :art: :white_check_mark: [ft] Test suite only with `expect`

---
+++ test/ft/test_suite_3.cpp
@@ -0,0 +1,13 @@
+//
+// Copyright (c) 2019 Kris Jusiak (kris at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+#include "test.hpp"
+
+static test::suite _ = [] {
+  using namespace test;
+  expect(type<int> == type<int>);
+};
