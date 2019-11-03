d2a09b82ca89d3f9f2a8f46eb775f25c36f66346
Kris Jusiak <kris@jusiak.net>
2019-11-02 20:18:38 -0600

    :new: :wrench: [ut] Introduce `ut` module using `ut.mpp`

---
+++ example/module.cpp
@@ -0,0 +1,15 @@
+//
+// Copyright (c) 2019 Kris Jusiak (kris at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+import std;
+import boost.ut;
+
+int main() {
+  using namespace boost::ut;
+
+  "module"_test = [] { expect(42_i == 42 and constant<3 == 3>); };
+}
