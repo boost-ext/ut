35351c4ba8a0f9d12af6c58d3b961628c203fcfe
Kris <kris@jusiak.net>
2019-11-14 17:11:20 -0600

    :racehorse: :art: [benchmark] Add suite benchmark

--- benchmark/expect.cpp
+++ benchmark/expect.cpp
@@ -10,7 +10,7 @@
 int main() {
   using namespace boost::ut;
 
-  "assert"_test = [] {
+  "expect"_test = [] {
     for (auto i = 0; i < 1'000'000; ++i) {
       expect(i == _i(i));
     }
---
+++ benchmark/suite.cpp
@@ -0,0 +1,152 @@
+//
+// Copyright (c) 2019 Kris Jusiak (kris at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+#include <boost/ut.hpp>
+
+namespace ut = boost::ut;
+
+ut::suite _1 = [] {
+  using namespace ut;
+  "1.1"_test = [] {};
+  "1.2"_test = [] {};
+  "1.3"_test = [] {};
+  "1.4"_test = [] {};
+  "1.5"_test = [] {};
+  "1.6"_test = [] {};
+  "1.7"_test = [] {};
+  "1.8"_test = [] {};
+  "1.9"_test = [] {};
+  "1.10"_test = [] {};
+};
+
+ut::suite _2 = [] {
+  using namespace ut;
+  "2.1"_test = [] {};
+  "2.2"_test = [] {};
+  "2.3"_test = [] {};
+  "2.4"_test = [] {};
+  "2.5"_test = [] {};
+  "2.6"_test = [] {};
+  "2.7"_test = [] {};
+  "2.8"_test = [] {};
+  "2.9"_test = [] {};
+  "2.10"_test = [] {};
+};
+
+ut::suite _3 = [] {
+  using namespace ut;
+  "3.1"_test = [] {};
+  "3.2"_test = [] {};
+  "3.3"_test = [] {};
+  "3.4"_test = [] {};
+  "3.5"_test = [] {};
+  "3.6"_test = [] {};
+  "3.7"_test = [] {};
+  "3.8"_test = [] {};
+  "3.9"_test = [] {};
+  "3.10"_test = [] {};
+};
+
+ut::suite _4 = [] {
+  using namespace ut;
+  "4.1"_test = [] {};
+  "4.2"_test = [] {};
+  "4.3"_test = [] {};
+  "4.4"_test = [] {};
+  "4.5"_test = [] {};
+  "4.6"_test = [] {};
+  "4.7"_test = [] {};
+  "4.8"_test = [] {};
+  "4.9"_test = [] {};
+  "4.10"_test = [] {};
+};
+
+ut::suite _5 = [] {
+  using namespace ut;
+  "5.1"_test = [] {};
+  "5.2"_test = [] {};
+  "5.3"_test = [] {};
+  "5.4"_test = [] {};
+  "5.5"_test = [] {};
+  "5.6"_test = [] {};
+  "5.7"_test = [] {};
+  "5.8"_test = [] {};
+  "5.9"_test = [] {};
+  "5.10"_test = [] {};
+};
+
+ut::suite _6 = [] {
+  using namespace ut;
+  "6.1"_test = [] {};
+  "6.2"_test = [] {};
+  "6.3"_test = [] {};
+  "6.4"_test = [] {};
+  "6.5"_test = [] {};
+  "6.6"_test = [] {};
+  "6.7"_test = [] {};
+  "6.8"_test = [] {};
+  "6.9"_test = [] {};
+  "6.10"_test = [] {};
+};
+
+ut::suite _7 = [] {
+  using namespace ut;
+  "7.1"_test = [] {};
+  "7.2"_test = [] {};
+  "7.3"_test = [] {};
+  "7.4"_test = [] {};
+  "7.5"_test = [] {};
+  "7.6"_test = [] {};
+  "7.7"_test = [] {};
+  "7.8"_test = [] {};
+  "7.9"_test = [] {};
+  "7.10"_test = [] {};
+};
+
+ut::suite _8 = [] {
+  using namespace ut;
+  "8.1"_test = [] {};
+  "8.2"_test = [] {};
+  "8.3"_test = [] {};
+  "8.4"_test = [] {};
+  "8.5"_test = [] {};
+  "8.6"_test = [] {};
+  "8.7"_test = [] {};
+  "8.8"_test = [] {};
+  "8.9"_test = [] {};
+  "8.10"_test = [] {};
+};
+
+ut::suite _9 = [] {
+  using namespace ut;
+  "9.1"_test = [] {};
+  "9.2"_test = [] {};
+  "9.3"_test = [] {};
+  "9.4"_test = [] {};
+  "9.5"_test = [] {};
+  "9.6"_test = [] {};
+  "9.7"_test = [] {};
+  "9.8"_test = [] {};
+  "9.9"_test = [] {};
+  "9.10"_test = [] {};
+};
+
+ut::suite _10 = [] {
+  using namespace ut;
+  "10.1"_test = [] {};
+  "10.2"_test = [] {};
+  "10.3"_test = [] {};
+  "10.4"_test = [] {};
+  "10.5"_test = [] {};
+  "10.6"_test = [] {};
+  "10.7"_test = [] {};
+  "10.8"_test = [] {};
+  "10.9"_test = [] {};
+  "10.10"_test = [] {};
+};
+
+int main() {}
