908c7f9952369b7c0d7510221acc84876424833a
Kris Jusiak <kris@jusiak.net>
2019-11-22 16:59:30 -0700

    :new: [example] macro

---
+++ example/macro.cpp
@@ -0,0 +1,40 @@
+//
+// Copyright (c) 2019 Kris Jusiak (kris at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+#include <boost/ut.hpp>
+#include <vector>
+
+#define EXPECT(...) ::boost::ut::expect(::boost::ut::that % __VA_ARGS__)
+#define SUITE ::boost::ut::suite _ = []
+#define TEST(name) ::boost::ut::detail::test{"test", name} = [=]() mutable
+#define TEST_P(name, ...)                 \
+  using namespace ::boost::ut::operators; \
+  ::boost::ut::detail::test{"test", name} = [=](__VA_ARGS__) mutable
+
+SUITE {
+  TEST("suite") { EXPECT(42 == 42); };
+
+  TEST_P("parameterized", const auto& arg) {
+    EXPECT(arg > 0) << "all values greater than 0";
+  }
+  | std::vector{1, 2, 3};
+};
+
+int main() {
+  TEST("macro") { EXPECT(1 != 2); };
+
+  TEST("vector") {
+    std::vector<int> v(5);
+
+    !EXPECT(5u == std::size(v));
+
+    TEST("resize bigger") {
+      v.resize(10);
+      EXPECT(10u == std::size(v));
+    };
+  };
+}
