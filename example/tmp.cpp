d72cbb65d41e165d6490f165d872b9924770410b
Kris Jusiak <kris@jusiak.net>
2019-12-03 14:23:58 -0700

    :new: [Example] TMP tests

---
+++ example/tmp.cpp
@@ -0,0 +1,22 @@
+//
+// Copyright (c) 2019 Kris Jusiak (kris at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+#include <boost/ut.hpp>
+
+template <class...>
+struct list {};
+
+int main() {
+  using namespace boost::ut;
+
+  static constexpr auto i = 42;
+
+  "tmp"_test = [] {
+    expect(constant<42_i == i> and type<void> == type<void> and
+           type<list<void, int>> == type<list<void, int>>);
+  };
+}
