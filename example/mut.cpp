5e0d65d419cda6a945f977b1664b50a0f7085d1a
Kris <kris@jusiak.net>
2019-12-05 15:02:04 -0600

    :new: [example] mut

---
+++ example/mut.cpp
@@ -0,0 +1,28 @@
+//
+// Copyright (c) 2019 Kris Jusiak (kris at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+#include <boost/ut.hpp>
+
+int main() {
+  using namespace boost::ut;
+
+  auto i = 0; // mutable
+
+  "mut"_test = [i] {
+    !expect(i == 0_i);
+
+    should("++") = [i] {
+      expect(++mut(i) == 1_i);
+      expect(i == 1_i);
+    };
+
+    should("--") = [i] {
+      expect(--mut(i) == -1_i);
+      expect(i == -1_i);
+    };
+  };
+}
