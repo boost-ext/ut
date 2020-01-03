08f4836fe08feddc9eb1fff02ad1b7a92fc060b9
Park DongHa <luncliff@gmail.com>
2020-01-04 02:27:47 +0900

    appveyor: ignore failed tests for review
    * lite version of suite
    
    suite failes to link when over 6 'suite' is defined.

---
+++ benchmark/suite_lite.cpp
@@ -0,0 +1,82 @@
+//
+// Copyright (c) 2019 Kris Jusiak (kris at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+#include <boost/ut.hpp>
+
+namespace ut = boost::ut;
+
+ut::suite _1 = [] {
+  using namespace ut;
+  "1.1"_test = [] {};
+  "1.2"_test = [] {};
+  "1.3"_test = [] {};
+  "1.4"_test = [] {};
+  "1.5"_test = [] {};
+  "1.6"_test = [] {};
+  "1.7"_test = [] {};
+  "1.8"_test = [] {};
+  "1.9"_test = [] {};
+  "1.10"_test = [] {};
+};
+
+ut::suite _2 = [] {
+  using namespace ut;
+  "2.1"_test = [] {};
+  "2.2"_test = [] {};
+  "2.3"_test = [] {};
+  "2.4"_test = [] {};
+  "2.5"_test = [] {};
+  "2.6"_test = [] {};
+  "2.7"_test = [] {};
+  "2.8"_test = [] {};
+  "2.9"_test = [] {};
+  "2.10"_test = [] {};
+};
+
+ut::suite _3 = [] {
+  using namespace ut;
+  "3.1"_test = [] {};
+  "3.2"_test = [] {};
+  "3.3"_test = [] {};
+  "3.4"_test = [] {};
+  "3.5"_test = [] {};
+  "3.6"_test = [] {};
+  "3.7"_test = [] {};
+  "3.8"_test = [] {};
+  "3.9"_test = [] {};
+  "3.10"_test = [] {};
+};
+
+ut::suite _4 = [] {
+  using namespace ut;
+  "4.1"_test = [] {};
+  "4.2"_test = [] {};
+  "4.3"_test = [] {};
+  "4.4"_test = [] {};
+  "4.5"_test = [] {};
+  "4.6"_test = [] {};
+  "4.7"_test = [] {};
+  "4.8"_test = [] {};
+  "4.9"_test = [] {};
+  "4.10"_test = [] {};
+};
+
+ut::suite _5 = [] {
+  using namespace ut;
+  "5.1"_test = [] {};
+  "5.2"_test = [] {};
+  "5.3"_test = [] {};
+  "5.4"_test = [] {};
+  "5.5"_test = [] {};
+  "5.6"_test = [] {};
+  "5.7"_test = [] {};
+  "5.8"_test = [] {};
+  "5.9"_test = [] {};
+  "5.10"_test = [] {};
+};
+
+int main() {}
