6710ac47e83ad5cd03d9e0505f19f8f8aba6249e
Kris Jusiak <kris@jusiak.net>
2020-07-16 15:10:08 -0600

    :bug: [UT] `floating-point` den size can overflow
    Problem:
    - `den_size` for floating point has to respect possible number space.
    
    Solution:
    - Use `unsigned long` `unsigned long long` for den_size with `""_d` and `""_ld`.

--- test/ut/ut.cpp
+++ test/ut/ut.cpp
@@ -239,7 +239,9 @@ int main() {
       static_assert(42u == 42_ul);
       static_assert(42.42f == 42.42_f);
       static_assert(42.42 == 42.42_d);
+      static_assert(240.209996948 == 240.209996948);
       static_assert(static_cast<long double>(42.42) == 42.42_ld);
+      static_assert(240.20999694824218 == 240.20999694824218_ld);
       static_assert(0 == 0_i);
       static_assert(10'000 == 10'000_i);
       static_assert(42000'000 == 42000'000_i);
