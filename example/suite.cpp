a1b6414dad0fa1ce169fe94610e98a223a5a590d
Kris Jusiak <kris@jusiak.net>
2022-09-30 10:49:27 +0000

    :new: [example] in-class test suite

--- example/suite.cpp
+++ example/suite.cpp
@@ -19,4 +19,22 @@ ut::suite errors = [] {
   "failure"_test = [] { expect(nothrow([] {})); };
 };
 
+#if not defined(__clang__) and not defined(_MSC_VER)
+struct calc {
+  static inline ut::suite sum_suite = []<class T = calc>()->void {
+    using namespace ut;
+
+    "sum"_test = [] {
+      T calc{};
+      expect(6_i == calc.sum(1, 2, 3));
+    };
+  };
+
+  template <class... TArgs>
+  constexpr auto sum(TArgs... args) -> int {
+    return (args + ...);
+  }
+};
+#endif
+
 int main() {}
