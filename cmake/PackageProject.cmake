4666404b922d3bfa4bf249fa4fb6b581731a4474
ClausKlein <claus.klein@arcormail.de>
2022-11-29 20:44:50 +0100

    Update cmake modules

--- cmake/PackageProject.cmake
+++ cmake/PackageProject.cmake
@@ -1,26 +1,4 @@
-# ------------------------------------------------------------------------------
-# MIT License
-#
-# Copyright (c) 2020 Lars Melchior
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-# ------------------------------------------------------------------------------
+cmake_minimum_required(VERSION 3.14)
 
 set(PACKAGE_PROJECT_ROOT_PATH
     "${CMAKE_CURRENT_LIST_DIR}"
@@ -34,7 +12,7 @@ function(packageProject)
   cmake_parse_arguments(
     PROJECT
     ""
-    "NAME;VERSION;INCLUDE_DIR;INCLUDE_DESTINATION;BINARY_DIR;COMPATIBILITY;EXPORT_HEADER;VERSION_HEADER;NAMESPACE;DISABLE_VERSION_SUFFIX"
+    "NAME;VERSION;INCLUDE_DIR;INCLUDE_DESTINATION;BINARY_DIR;COMPATIBILITY;EXPORT_HEADER;VERSION_HEADER;NAMESPACE;DISABLE_VERSION_SUFFIX;ARCH_INDEPENDENT;INCLUDE_HEADER_PATTERN"
     "DEPENDENCIES"
     ${ARGN}
   )
@@ -42,13 +20,12 @@ function(packageProject)
   # optional feature: TRUE or FALSE or UNDEFINED! These variables will then hold the respective
   # value from the argument list or be undefined if the associated one_value_keyword could not be
   # found.
-  if(DEFINED PROJECT_DISABLE_VERSION_SUFFIX AND PROJECT_DISABLE_VERSION_SUFFIX)
+  if(PROJECT_DISABLE_VERSION_SUFFIX)
     unset(PROJECT_VERSION_SUFFIX)
   else()
     set(PROJECT_VERSION_SUFFIX -${PROJECT_VERSION})
   endif()
 
-  # handle default arguments:
   if(NOT DEFINED PROJECT_COMPATIBILITY)
     set(PROJECT_COMPATIBILITY AnyNewerVersion)
   endif()
@@ -70,6 +47,32 @@ function(packageProject)
     endif()
 
     if(DEFINED PROJECT_VERSION_HEADER)
+      # clear previous matches
+      unset(CMAKE_MATCH_1)
+      unset(CMAKE_MATCH_3)
+      unset(CMAKE_MATCH_5)
+      unset(CMAKE_MATCH_7)
+
+      string(REGEX MATCH "^([0-9]+)(\\.([0-9]+))?(\\.([0-9]+))?(\\.([0-9]+))?$" _ "${PROJECT_VERSION}")
+
+      set(PROJECT_VERSION_MAJOR ${CMAKE_MATCH_1})
+      set(PROJECT_VERSION_MINOR ${CMAKE_MATCH_3})
+      set(PROJECT_VERSION_PATCH ${CMAKE_MATCH_5})
+      set(PROJECT_VERSION_TWEAK ${CMAKE_MATCH_7})
+
+      if(NOT DEFINED PROJECT_VERSION_MAJOR)
+        set(PROJECT_VERSION_MAJOR "0")
+      endif()
+      if(NOT DEFINED PROJECT_VERSION_MINOR)
+        set(PROJECT_VERSION_MINOR "0")
+      endif()
+      if(NOT DEFINED PROJECT_VERSION_PATCH)
+        set(PROJECT_VERSION_PATCH "0")
+      endif()
+      if(NOT DEFINED PROJECT_VERSION_TWEAK)
+        set(PROJECT_VERSION_TWEAK "0")
+      endif()
+
       string(TOUPPER ${PROJECT_NAME} UPPERCASE_PROJECT_NAME)
       configure_file(${PACKAGE_PROJECT_ROOT_PATH}/version.h.in ${PROJECT_VERSION_INCLUDE_DIR}/${PROJECT_VERSION_HEADER} @ONLY)
     endif()
@@ -81,41 +84,80 @@ function(packageProject)
       set(VISIBILITY PUBLIC)
     endif()
     target_include_directories(${PROJECT_NAME} ${VISIBILITY} "$<BUILD_INTERFACE:${PROJECT_VERSION_INCLUDE_DIR}>")
-    install(DIRECTORY ${PROJECT_VERSION_INCLUDE_DIR}/ DESTINATION ${PROJECT_INCLUDE_DESTINATION})
+    install(
+      DIRECTORY ${PROJECT_VERSION_INCLUDE_DIR}/
+      DESTINATION ${PROJECT_INCLUDE_DESTINATION}
+      COMPONENT "${PROJECT_NAME}_Development"
+    )
+  endif()
+
+  set(wbpvf_extra_args "")
+  if(NOT DEFINED PROJECT_ARCH_INDEPENDENT)
+    get_target_property(target_type "${PROJECT_NAME}" TYPE)
+    if(TYPE STREQUAL "INTERFACE_LIBRARY")
+      set(PROJECT_ARCH_INDEPENDENT YES)
+    endif()
+  endif()
+
+  if(PROJECT_ARCH_INDEPENDENT)
+    set(wbpvf_extra_args ARCH_INDEPENDENT)
   endif()
 
   write_basic_package_version_file(
     "${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake"
     VERSION ${PROJECT_VERSION}
-    COMPATIBILITY ${PROJECT_COMPATIBILITY}
+    COMPATIBILITY ${PROJECT_COMPATIBILITY} ${wbpvf_extra_args}
   )
 
   install(
     TARGETS ${PROJECT_NAME}
     EXPORT ${PROJECT_NAME}Targets
-    LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}/${PROJECT_NAME}${PROJECT_VERSION_SUFFIX} COMPONENT Runtime
-    ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}/${PROJECT_NAME}${PROJECT_VERSION_SUFFIX} COMPONENT Development
-    RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}/${PROJECT_NAME}${PROJECT_VERSION_SUFFIX} COMPONENT Runtime
-    BUNDLE DESTINATION ${CMAKE_INSTALL_BINDIR}/${PROJECT_NAME}${PROJECT_VERSION_SUFFIX} COMPONENT Runtime
-    PUBLIC_HEADER DESTINATION ${PROJECT_INCLUDE_DESTINATION} COMPONENT Development
+    LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}/${PROJECT_NAME}${PROJECT_VERSION_SUFFIX}
+            COMPONENT "${PROJECT_NAME}_Runtime"
+            NAMELINK_COMPONENT "${PROJECT_NAME}_Development"
+    ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}/${PROJECT_NAME}${PROJECT_VERSION_SUFFIX} COMPONENT "${PROJECT_NAME}_Development"
+    RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}/${PROJECT_NAME}${PROJECT_VERSION_SUFFIX} COMPONENT "${PROJECT_NAME}_Runtime"
+    BUNDLE DESTINATION ${CMAKE_INSTALL_BINDIR}/${PROJECT_NAME}${PROJECT_VERSION_SUFFIX} COMPONENT "${PROJECT_NAME}_Runtime"
+    PUBLIC_HEADER DESTINATION ${PROJECT_INCLUDE_DESTINATION} COMPONENT "${PROJECT_NAME}_Development"
+    INCLUDES
+    DESTINATION "${PROJECT_INCLUDE_DESTINATION}"
   )
 
-  configure_package_config_file(
-    ${PACKAGE_PROJECT_ROOT_PATH}/Config.cmake.in "${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake"
-    INSTALL_DESTINATION lib/cmake/${PROJECT_NAME}${PROJECT_VERSION_SUFFIX}
+  set("${PROJECT_NAME}_INSTALL_CMAKEDIR"
+      "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}${PROJECT_VERSION_SUFFIX}"
+      CACHE PATH "CMake package config location relative to the install prefix"
   )
+
+  mark_as_advanced("${PROJECT_NAME}_INSTALL_CMAKEDIR")
+
+  configure_file(${PACKAGE_PROJECT_ROOT_PATH}/Config.cmake.in "${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake" @ONLY)
 
   install(
     EXPORT ${PROJECT_NAME}Targets
-    DESTINATION lib/cmake/${PROJECT_NAME}${PROJECT_VERSION_SUFFIX}
+    DESTINATION "${${PROJECT_NAME}_INSTALL_CMAKEDIR}"
     NAMESPACE ${PROJECT_NAMESPACE}
+    COMPONENT "${PROJECT_NAME}_Development"
   )
 
-  install(FILES "${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake" "${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake"
-          DESTINATION lib/cmake/${PROJECT_NAME}${PROJECT_VERSION_SUFFIX}
+  install(
+    FILES "${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake" "${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake"
+    DESTINATION "${${PROJECT_NAME}_INSTALL_CMAKEDIR}"
+    COMPONENT "${PROJECT_NAME}_Development"
   )
 
-  install(DIRECTORY ${PROJECT_INCLUDE_DIR}/ DESTINATION ${PROJECT_INCLUDE_DESTINATION})
+  if(NOT DEFINED PROJECT_INCLUDE_HEADER_PATTERN)
+    set(PROJECT_INCLUDE_HEADER_PATTERN "*")
+  endif()
+
+  if(PROJECT_INCLUDE_DESTINATION AND PROJECT_INCLUDE_DIR)
+    install(
+      DIRECTORY ${PROJECT_INCLUDE_DIR}/
+      DESTINATION ${PROJECT_INCLUDE_DESTINATION}
+      COMPONENT "${PROJECT_NAME}_Development"
+      FILES_MATCHING
+      PATTERN "${PROJECT_INCLUDE_HEADER_PATTERN}"
+    )
+  endif()
 
   set(${PROJECT_NAME}_VERSION
       ${PROJECT_VERSION}
