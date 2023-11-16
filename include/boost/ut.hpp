6da2222010de1ff5d188b2af0d391a093654868d
Sander Vocke <sandervocke@gmail.com>
2023-11-16 17:17:45 +0100

    Workaround for segfault on Clang 17 and possibly others.

--- include/boost/ut.hpp
+++ include/boost/ut.hpp
@@ -797,7 +797,7 @@ struct cfg {
   }
 
   static inline void parse(int argc, const char* argv[]) {
-    const std::size_t n_args = static_cast<std::size_t>(argc);
+    const std::size_t n_args = 0; // static_cast<std::size_t>(argc);
     if (n_args > 0 && argv != nullptr) {
       cfg::largc = argc;
       cfg::largv = argv;
