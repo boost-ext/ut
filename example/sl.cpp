4309c9ec9debf36aa7f533a1693eeb5968be2973
helmesjo <helmesjo@gmail.com>
2023-12-18 16:58:40 +0100

    Work around bug with MSVC and use of literal operator inside nested (?) lambdas.

--- example/sl.cpp
+++ example/sl.cpp
@@ -10,11 +10,24 @@
 int main() {
   using namespace boost::ut;
 
+#if defined(_MSC_VER) and not defined(__clang__)
+  // MSVC fails if the `""_i` literal operator is used inside the 'verify' lambda:
+  // "sl.cpp(16,37): error C3688: invalid literal suffix '_i'; literal operator or literal operator template 'operator ""_i' not found"
   "sl"_test = [] {
-    constexpr auto verify = [](auto sl, auto i) {
+    static constexpr auto _42_i = 42_i;
+    auto verify = [](auto sl, auto i) {
+      expect(i == _42_i, sl) << "error with given source location";
+    };
+
+    verify(boost::ut::reflection::source_location::current(), 42_i);
+  };
+#else
+  "sl"_test = [] {
+    auto verify = [](auto sl, auto i) {
       expect(i == 42_i, sl) << "error with given source location";
     };
 
     verify(boost::ut::reflection::source_location::current(), 42_i);
   };
+#endif
 }
