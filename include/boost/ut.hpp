0f8d85da51a417c9681fe5f26bd16c2585cb4d24
Kris Jusiak <kris@jusiak.net>
2023-12-18 13:10:09 -0600

    :arrow_up: v2.0.1

--- include/boost/ut.hpp
+++ include/boost/ut.hpp
@@ -57,7 +57,7 @@ export import std;
 #elif not defined(__cpp_static_assert)
 #error "[Boost::ext].UT requires support for static assert";
 #else
-#define BOOST_UT_VERSION 2'0'0
+#define BOOST_UT_VERSION 2'0'1
 
 #if defined(__has_builtin) and defined(__GNUC__) and (__GNUC__ < 10) and \
     not defined(__clang__)
@@ -109,7 +109,7 @@ struct _unique_name_for_auto_detect_prefix_and_suffix_lenght_0123456789_struct {
 };
 
 BOOST_UT_EXPORT
-namespace boost::inline ext::ut::inline v2_0_0 {
+namespace boost::inline ext::ut::inline v2_0_1 {
 namespace utility {
 template <class>
 class function;
@@ -3276,7 +3276,7 @@ using operators::operator not;
 using operators::operator|;
 using operators::operator/;
 using operators::operator>>;
-}  // namespace boost::inline ext::ut::inline v2_0_0
+}  // namespace boost::inline ext::ut::inline v2_0_1
 
 #if (defined(__GNUC__) || defined(__clang__) || defined(__INTEL_COMPILER)) && \
     !defined(__EMSCRIPTEN__)
