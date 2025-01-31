3325acc53257322d8b5966e393d2dd9bae6667ea
Tobias Leibner <7058290+tobiasleibner@users.noreply.github.com>
2025-01-31 12:56:21 +0100

    Work around msvc compilation failures

--- cmake/AddCustomCommandOrTest.cmake
+++ cmake/AddCustomCommandOrTest.cmake
@@ -1,4 +1,4 @@
-option(BOOST_UT_ENABLE_RUN_AFTER_BUILD "Automatically run built artifacts. If disabled, the tests can be run with ctest instead" OFF)
+option(BOOST_UT_ENABLE_RUN_AFTER_BUILD "Automatically run built artifacts. If disabled, the tests can be run with ctest instead" ON)
 
 function(ut_add_custom_command_or_test)
   # Define the supported set of keywords
