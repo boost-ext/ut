00509706d28756830461c3e2ea0903a22c37b58a
Tobias Leibner <7058290+tobiasleibner@users.noreply.github.com>
2025-02-11 11:56:40 +0100

    Add more pre-commit hooks

--- test/test.sh
+++ test/test.sh
@@ -5,5 +5,3 @@ cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release -DFETCHCONTENT_QUIET=OFF --log-leve
 cmake --build ./build/ -j24 --verbose
 cmake --install ./build/ --prefix ./install_dir
 CMAKE_PREFIX_PATH=./install_dir/ cmake -Stest -Bbuild2 -DTEST_INSTALLED_VERSION=1 -DCPM_SOURCE_CACHE=~/.cache/cpm --log-level=DEBUG
-
-
