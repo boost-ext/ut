09cfe0659f634e5403a5a87e6bab97676d4f60e0
Ralph J. Steinhagen <r.steinhagen@gsi.de>
2025-03-04 11:14:25 +0100

    Fix ull->size_t conversion warning
    detected via the 'warning-as-error' flag:
    On many architectures `unsigned long long' is identical to 'std::size_t` but not, for example, on all x86_32, WASM, some MSVV versions.

--- include/boost/ut.hpp
+++ include/boost/ut.hpp
@@ -820,7 +820,7 @@ struct cfg {
         // parse size argument
         std::size_t last;
         std::string argument(argv[i]);
-        std::size_t val = std::stoull(argument, &last);
+        auto val = static_cast<std::size_t>(std::stoull(argument, &last));
         if (last != argument.length()) {
           std::cerr << "cannot parse option of " << argv[i - 1] << " "
                     << argv[i] << std::endl;
